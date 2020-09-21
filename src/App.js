32842d04054911b2cc48000f1b91bbce32840e9a
shiv-source <shivk7466@gmail.com>
2020-09-21 22:39:26 +0530

    header and footer component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/HeaderComponent';
-import Main from './components/MainComponent'
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.css";
+import "bootstrap-social/bootstrap-social.css";
+import Main from "./components/MainComponent";
+import Header from "./components/HeaderComponent";
+import Footer from "./components/FooterComponent";
+import "./App.css";
+
 class App extends Component {
- 
-  render(){
-
-    return(
+  render() {
+    return (
       <div>
-        <Header></Header>
-        <Main></Main>
+        <Header />
+        <Main />
+        <Footer />
       </div>
-    )
+    );
   }
-
 }
-
 
 export default App;
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,16 +1,27 @@
 import React , { Component } from 'react';
-import { Navbar , NavbarBrand  } from 'reactstrap';
+import { Navbar , NavbarBrand ,Jumbotron } from 'reactstrap';
 
 class Header extends Component {
 
     render(){
         return(
-            <div> 
-            <Navbar color="primary" light expand="md">
-                <NavbarBrand href="/" className="ml-5"> Food Strore </NavbarBrand> 
+            <React.Fragment>
+            <Navbar dark>
+              <div className="container">
+                  <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+              </div>
             </Navbar>
-
-            </div>
+            <Jumbotron>
+                 <div className="container">
+                     <div className="row row-header">
+                         <div className="col-12 col-sm-6">
+                             <h1>Ristorante con Fusion</h1>
+                             <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
+                         </div>
+                     </div>
+                 </div>
+             </Jumbotron>
+          </React.Fragment>
         )
     }
 }
