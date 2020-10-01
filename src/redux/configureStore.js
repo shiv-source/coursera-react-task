66905df8913f91bbde0460e94edf015bcabc0c58
shiv-source <shivk7466@gmail.com>
2020-10-01 20:31:14 +0530

    combining reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,18 @@
-import { createStore } from 'redux';
-import { Reducer , initialState } from './reducer';
+import { createStore, combineReducers } from "redux";
+import { Dishes } from "../redux/reducers/dishes";
+import { Comments } from "../redux/reducers/commetns";
+import { Promotions } from "../redux/reducers/promotions";
+import { Leaders } from "../redux/reducers/leaders";
 
-export const ConfigureStore = ()  => {
-    const store = createStore(
-        Reducer,
-        initialState,
+export const ConfigureStore = () => {
+  const store = createStore(
+    combineReducers({
+      dishes: Dishes,
+      comments: Comments,
+      promotions: Promotions,
+      leaders: Leaders,
+    })
+  );
 
-    );
-
-    return store;
-    
-}
+  return store;
+};
