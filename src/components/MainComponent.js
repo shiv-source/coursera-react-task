18dbaacce6cc2512a6445c3355e4b9ecaadfd305
shiv-source <shivk7466@gmail.com>
2020-10-06 10:31:26 +0530

    React Animations

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import About from "./AboutComponent";
 import { connect } from "react-redux";
 import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
 import { actions } from "react-redux-form";
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const mapStateToProps = (state) => {
   return {
@@ -68,6 +69,8 @@ class Main extends Component {
     return (
       <div>
         <Header />
+        <TransitionGroup>
+        <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route
@@ -95,6 +98,8 @@ class Main extends Component {
           />
           <Redirect to="/home" />
         </Switch>
+        </CSSTransition>
+        </TransitionGroup>
         <Footer />
       </div>
     );
